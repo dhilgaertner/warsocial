9e533bfc83676a27ff46d141d8489ce7e20ba4f6
Dustin Hilgaertner <dhilgaertner@dustin-hilgaertners-macbook-pro.local>
2011-11-20 04:42:28 -0500

    dice

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dice::Application.config.secret_token = '6c3d77672a3e06dc3848e29f32e903bc600eedb873d09e108a04c45472b4c81a3bbd3cc9ef4bcf6e53b0ed5fccf89d4d66acaf52d185b224be9f6a6e26a49d4a'
