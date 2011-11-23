1283b481d639db69f49670981e38a7b9e6608cad
Dustin Hilgaertner <dhilgaertner@dustin-hilgaertners-macbook-pro.local>
2011-11-23 16:11:55 -0500

    OmniAuth callback / integration

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,27 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def passthru
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
+  
+  def facebook
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+  
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token.extra.raw_info
+    if user = User.find_by_email(data.email)
+      user
+    else # Create a user with a stub password. 
+      User.create!(:email => data.email, :encrypted_password => Devise.friendly_token[0,20]) 
+    end
+  end
+  
+end
