a22134587d9152e9373ff50c18dc52b7f3bf659e
Dustin Hilgaertner <dhilgaertner@dustin-hilgaertners-macbook-pro.local>
2011-11-23 12:50:41 -0500

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Dice::Application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
