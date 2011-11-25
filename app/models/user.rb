8b4cc0330af240ee539dc98f3f015fefbf597a2d
Dustin Hilgaertner <dhilgaertner@dustin-hilgaertners-macbook-pro.local>
2011-11-25 02:42:07 -0500

    logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,24 +15,20 @@ class User < ActiveRecord::Base
   def self.find_for_oauth(access_token, signed_in_resource=nil)
     logger.info access_token
     
-    data = access_token.extra.raw_info
+    data = access_token.info
     puts access_token.inspect
     if user = User.find_by_email(data.email)
       user
     else # Create a user with a stub password. 
-      User.create!(:email => data.email, :username => data.username, :password => Devise.friendly_token[0,20]) 
+      User.create!(:email => data.email, :username => data.name, :password => Devise.friendly_token[0,20]) 
     end
   end
   
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["user_hash"]
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["info"]
         user.email = data["email"]
-        user.username = data["username"]
-      end
-      logger.info session["devise.twitter_data"]
-      if data = session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["user_hash"]
-        user.email = data["email"]
+        user.username = data["name"]
       end
     end
   end
