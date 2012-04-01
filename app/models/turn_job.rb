aac30d47e30db03b4353d09409af3efd05b44727
dhilgaertner <dhilgaertner@gmail.com>
2012-04-01 12:13:08 -0400

    end turn is functioning now

--- app/models/turn_job.rb
+++ app/models/turn_job.rb
@@ -2,14 +2,10 @@ class TurnJob < Struct.new(:game_name)
   def perform
     require 'net/http'
 
-    Delayed::Worker.logger.info self.game_name
-
-    game = Game.get_game(self.game_name)
-
     base_url = Rails.env.production? ? "http://www.warsocial.com/" : "http://localhost:3000/"
     
     # url = URI.join(base_url,'home/force_end_turn')
-    url = URI.join(base_url,'game/' + game.name + '/fet/whisper')
+    url = URI.join(base_url,'game/' + self.game_name + '/fet/whisper')
     req = Net::HTTP::Get.new(url.path)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
