0ce673661a87fcba6551a75b823eaf3050070faf
dhilgaertner <dhilgaertner@gmail.com>
2012-06-01 17:51:14 -0400

    new sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable#, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :forem_admin,
+  attr_accessible :email, :password, :remember_me, :username, :forem_admin,
                   :current_points, :total_points
   
   validates_presence_of :email
