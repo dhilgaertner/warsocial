493701b694aff37e7824d2f81526d71b35160173
dhilgaertner <dhilgaertner@gmail.com>
2012-06-10 00:43:44 -0400

    forfeit button + better progress bar

---
+++ app/models/active/active_stats.rb
@@ -0,0 +1,26 @@
+class ActiveStats
+
+  attr_accessor :positions
+
+  def initialize()
+
+  end
+
+  def save
+
+  end
+
+  def delete
+
+  end
+
+  def as_json(options={})
+
+  end
+
+  # helper method to generate redis id
+  def id
+
+  end
+
+end
