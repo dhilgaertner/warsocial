9e8694a19f91e045f02d0c2da3fae5914d88882d
dhilgaertner <dhilgaertner@gmail.com>
2012-07-08 12:18:34 -0400

    unauthorized response for user_settings; bug fix

--- app/controllers/user_settings_controller.rb
+++ app/controllers/user_settings_controller.rb
@@ -1,30 +1,42 @@
 class UserSettingsController < ApplicationController
 
   def toggle_stats
-    is_on = params[:on].to_s == "true"
-    user = ActiveUser.get_active_user(current_user.id)
-    user.stats_toggle = is_on
-    user.save
+    if (current_user != nil)
+      is_on = params[:on].to_s == "true"
+      user = ActiveUser.get_active_user(current_user.id)
+      user.stats_toggle = is_on
+      user.save
 
-    render :text=>"saved", :status=>200
+      render :text=>"saved", :status=>200
+    else
+      render :text=>"unauthorized", :status=>403
+    end
   end
 
   def toggle_sounds
-    is_on = params[:on].to_s == "true"
-    user = ActiveUser.get_active_user(current_user.id)
-    user.sounds_toggle = is_on
-    user.save
+    if (current_user != nil)
+      is_on = params[:on].to_s == "true"
+      user = ActiveUser.get_active_user(current_user.id)
+      user.sounds_toggle = is_on
+      user.save
 
-    render :text=>"saved", :status=>200
+      render :text=>"saved", :status=>200
+    else
+      render :text=>"unauthorized", :status=>403
+    end
   end
 
   def toggle_layout
-    layout_id = params[:layout_id].to_i
-    user = ActiveUser.get_active_user(current_user.id)
-    user.layout_id = layout_id
-    user.save
+    if (current_user != nil)
+      layout_id = params[:layout_id].to_i
+      user = ActiveUser.get_active_user(current_user.id)
+      user.layout_id = layout_id
+      user.save
 
-    render :text=>"saved", :status=>200
+      render :text=>"saved", :status=>200
+    else
+      render :text=>"unauthorized", :status=>403
+    end
   end
 
 end
