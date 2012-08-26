184aaccd1b545081ac96042301c4e1726f9b18a1
dhilgaertner <dhilgaertner@gmail.com>
2012-08-25 23:56:57 -0400

    oops

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,15 +25,6 @@ class User < ActiveRecord::Base
   def medals
     places = Array.new
     self.season_scores.order("created_at").each do |score|
-      places.push(score.season.season_scores.count(:conditions => ['points > ?', score.points]) + 1)
-    end
-
-    return places
-  end
-
-  def medals
-    places = Array.new
-    self.season_scores.order("created_at").each do |score|
       if (score.points > 0)
         places.push(score.season.season_scores.count(:conditions => ['points > ?', score.points]) + 1)
       end
