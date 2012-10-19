1f16b803240a324454b0109f68a6215559a0a49e
dhilgaertner <dhilgaertner@gmail.com>
2012-10-19 18:09:05 -0400

    Added sections to profiles; tab control; layout

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,9 @@
 class ProfileController < ApplicationController
 
   def index
+    username = params[:username]
+    @user = User.find_by_username(username)
+
     render :action => "index", :layout => "application2"
   end
 
