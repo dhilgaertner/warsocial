98fc14b78cba38ca955544ac3df64808d6e4e4c2
dhilgaertner <dhilgaertner@gmail.com>
2012-10-27 22:18:55 -0400

    more changes to the admin page

--- app/admin/archived_players.rb
+++ app/admin/archived_players.rb
@@ -1,14 +1,6 @@
 require 'archived/archived_game'
 require 'archived/archived_player'
 
-ActiveAdmin.register ArchivedGame do
-  index do
-    column "Id", :id
-    column "Name", :name
-    column "Wager", :wager
-    column "Type", :game_type
-    column "Map", :map_name
-    column "Players", :archived_players
-    default_actions
-  end
+ActiveAdmin.register ArchivedPlayer do
+
 end
