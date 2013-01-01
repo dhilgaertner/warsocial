406cc8ae3ac0a5f148f1db741b0b21ddecc6cf6b
dhilgaertner <dhilgaertner@gmail.com>
2013-01-01 00:58:36 -0500

    setup of views and controller actions for map market

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,3 +1,13 @@
 class MapsController < ApplicationController
 
+  def index
+
+    render :action => "index", :layout => "application2"
+  end
+
+  def detail
+
+    render :action => "detail", :layout => "application2"
+  end
+
 end
