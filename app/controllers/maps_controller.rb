c2e10d4c7e7cd1777b75664b1d8846200414f57a
dhilgaertner <dhilgaertner@gmail.com>
2013-01-03 00:45:46 -0500

    progress on map market index

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,10 +2,16 @@ class MapsController < ApplicationController
 
   def index
 
+    @maps = Map.all
+    @map_counts = ArchivedGame.map_usage_counts
+
     render :action => "index", :layout => "application2"
   end
 
   def detail
+    map_id = params[:map_id]
+
+    @map = Map.find(map_id.to_i)
 
     render :action => "detail", :layout => "application2"
   end
