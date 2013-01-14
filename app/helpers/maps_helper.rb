bf27fdd2c12a3c0497e4bf312ace6fe235d1b85e
dhilgaertner <dhilgaertner@gmail.com>
2013-01-13 23:46:46 -0500

    added sorting, filtering, and animation

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -4,4 +4,14 @@ module MapsHelper
     return votes[vote].include?(map_id.to_s) ? "active" : ""
   end
 
+  def map_vote_class(votes, map_id)
+    if votes[0].include?(map_id.to_s)
+      return "thumb-down"
+    elsif votes[1].include?(map_id.to_s)
+      return "thumb-up"
+    else
+      return ""
+    end
+  end
+
 end
