ac15b9c093dd44043066e97f7a1452209065dcb7
dhilgaertner <dhilgaertner@gmail.com>
2013-01-20 18:26:08 -0500

    better tool tips + numbers of favorites and votes

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -21,4 +21,16 @@ module MapsHelper
       return "add-favorite"
     end
   end
+
+  def map_tooltip_favorite(num_favorites)
+    return "Favorite / Un-favorite this map. <br> #{num_favorites == nil ? "0" : num_favorites} user(s) have favorited this map."
+  end
+
+  def map_tooltip_like(num_like)
+    return "Vote FOR this map. <br> #{num_like == nil ? "0" : num_like} user(s) have voted FOR this map."
+  end
+
+  def map_tooltip_unlike(num_unlike)
+    return "Vote AGAINST this map. <br> #{num_unlike == nil ? "0" : num_unlike} user(s) have voted AGAINST this map."
+  end
 end
