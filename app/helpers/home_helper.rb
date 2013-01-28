350c9bb4b8cb5085eb4a297e2edf1603ac332ebb
dhilgaertner <dhilgaertner@gmail.com>
2013-01-27 19:14:38 -0500

    data + admin only for now

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -15,4 +15,15 @@ module HomeHelper
       return active_user.send(setting_name) ? 'checked="checked"' : ''
     end
   end
+
+  def vote_percentage(vote)
+    v1 = vote
+    v2 = vote == 0 ? 1 : 0
+
+    if (@map_votes[v1] == 0)
+      return 0
+    else
+      return ((@map_votes[v1] + @map_votes[v2]) / @map_votes[v1]) * 100
+    end
+  end
 end
