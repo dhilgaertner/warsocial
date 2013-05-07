225e32f73f6de505202bf82fe8bb9242d031b8ec
dhilgaertner <dhilgaertner@gmail.com>
2013-05-07 01:30:19 -0400

    undo

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -23,11 +23,3 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
       ActiveRecord::Base.establish_connection
 end
-
-# Ensure the agent is started using Unicorn.
-# This is needed when using Unicorn and preload_app is not set to true.
-# See https://newrelic.com/docs/ruby/no-data-with-unicorn
-if defined? Unicorn
-  ::NewRelic::Agent.manual_start()
-  ::NewRelic::Agent.after_fork(:force_reconnect => true)
-end
