71cac8aeb462aca62e807de43782e611f07f4265
dhilgaertner <dhilgaertner@gmail.com>
2014-02-23 12:47:26 -0500

    added fix for devise update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -92,10 +92,11 @@ Devise.setup do |config|
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
 
-  # If true, uses the password salt as remember token. This should be turned
+  # DEPRECATED: If true, uses the password salt as remember token. This should be turned
   # to false if you are not using database authenticatable.
-  config.use_salt_as_remember_token = true
+  # config.use_salt_as_remember_token = true
 
+  # Options to be passed to the created cookie. For instance, you can set
   # Options to be passed to the created cookie. For instance, you can set
   # :secure => true in order to force SSL only cookies.
   # config.cookie_options = {}
@@ -213,4 +214,6 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
+
+  config.secret_key = 'GXKbz5GSs6H73HrW14UR4kUrCxEEBR9Q'
 end
