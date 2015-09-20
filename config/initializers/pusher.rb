c4c91fca5c9357aa4b1044ad22c96741106f5b90
Dustin Hilgaertner <dhilgaertner@gmail.com>
2015-09-20 13:07:10 -0500

    Update pusher.rb

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,3 @@
-# Pusher.app_id = '11347'
-# Pusher.key    = 'f85901afd3e9eaff226f'
-# Pusher.secret = '5a1125ef70870d0d463c'
+# Pusher.app_id = '**APPID HERE**'
+# Pusher.key    = '**KEY HERE**'
+# Pusher.secret = '**SECRET HERE**'
